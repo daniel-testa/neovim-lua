2f4d10abd366a4b30ea01b53ed58d8ce386447c7
daniel testa <daniel.testa.t@gmail.com>
2024-11-17 19:51:00 -0300

    cambio: rainbow del

---
+++ lua/plugins/rainbow-delimiters.lua
@@ -0,0 +1,9 @@
+return {
+	"HiPhish/rainbow-delimiters.nvim",
+	event = "VeryLazy",
+	config = function()
+		local rainbow_delimiters = require("rainbow-delimiters")
+	end,
+}
+
+-- vim: ts=2 tw=2 sts=2 et
